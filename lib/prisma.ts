fd59557d7e156009df59f0cb89792481710dd7f3
Imunury <dipblu0808@gmail.com>
2025-02-27 11:22:15 +0900

    prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+// lib/prisma.ts
+import { PrismaClient } from "../generated/client";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-// Prisma 클라이언트 싱글톤 패턴 적용
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
--- lib/prisma_jincheon2.ts
+++ lib/prisma_jincheon2.ts
@@ -1,11 +1,6 @@
 // lib/prisma.ts
 import { PrismaClient } from "../generated/client_jincheon2";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-// Prisma 클라이언트 싱글톤 패턴 적용
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
